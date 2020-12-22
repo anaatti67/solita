0d0e00a19fe347b16212e62305368c5141283441
anaatti67 <51318599+anaatti67@users.noreply.github.com>
2020-12-22 23:51:21 +0200

    sdfsd

---
+++ src/app/requirements/one/one.ts
@@ -0,0 +1,31 @@
+
+import { Component } from '@angular/core';
+
+import nameData from '../../names/names.json'
+
+
+
+interface INames {
+		name: string,
+		amount: number
+}
+
+const { names } = nameData
+
+@Component({
+	selector: 'app-one',
+	templateUrl: './one.html'
+})
+
+
+
+export class OneComponent {
+	nameArray: Array<INames> = names
+
+	constructor() {
+		//Sorting names in descending order from name array.
+		var sorted = this.nameArray.sort((a, b) => (a.amount < b.amount) ? 1 : -1)
+
+		console.log(sorted) sdkfksdf
+	}
+}
